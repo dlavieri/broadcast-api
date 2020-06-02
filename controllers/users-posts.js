649cd5cc53f40b291df1411e1b61d1a626c84cc1
david lavieri <dvlavieri@gmail.com>
2020-06-02 15:44:10 -0700

    fix user-data-posts endpoint

--- controllers/users-posts.js
+++ controllers/users-posts.js
@@ -5,9 +5,8 @@ module.exports = (req, res, next) => {
     let username = req.params.username;
 
     User.findAll({where: {username: username}, include: [{model: Post}] })
-        .then(user => {
-            console.log(user)
-            return res.json(user)
+        .then(users => {
+            return res.json(users[0])
         })
         .catch(err => res.status(500).json({error: err}))
 }
